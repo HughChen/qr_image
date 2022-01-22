9cb36829ddce563c1ad694d19223cc3a78a92945
Hugh Chen <hugh.chen1@gmail.com>
2022-01-22 11:05:38 -0800

    Add support for smaller bit sizes

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 var qrcode = new QRCode("qrcode", {
-    // text: "http://jindo.dev.naver.com/collie",
-    width: 256,
-    height: 256,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+  width: 256,
+  height: 256,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H,
 });
 
-function makeCode () {
+/**
+ * Check whether bit at current position should be full sized.
+ *
+ * @param {i} The current bit's row.
+ * @param {j} The current bit's column.
+ * @param {QRLength} The length of the QR code.
+ * @return {isPosition} Whether or not the current bit is safe to modify.
+ */
+function isSafeBit(i, j, QRLength) {
 
+  // Currently hard coding position bits
+  if (i < 8 && j < 8) {
+    return false;
+  } else if (i > QRLength - 9 && j < 8) {
+    return false;
+  } else if (i < 8 && j > QRLength - 9) {
+    return false;
+  } 
+
+  return true;
+}
+
+/**
+ * Draw basic shape representing each bit of the QR code.
+ *
+ * @param {ctx} Context of associated canvas.
+ * @param {i} The current bit's row.
+ * @param {j} The current bit's column.
+ * @param {bitLength} The maximum length of each bit.
+ * @param {radiusRatio} The radius should be this ratio times the bitLength.
+ *  The ratio should be between 0 and 0.5.
+ * @param {QRLength} The length of the QR code.
+ */
+function drawShape(ctx, i, j, bitLength, radiusRatio, QRLength) {
+  // Draw centered
+  xCenter = bitLength * (i + 0.5);
+  yCenter = bitLength * (j + 0.5);
+
+  if (!isSafeBit(i, j, QRLength)) {
+    radiusRatio = 0.5;
+  }
+  radius = bitLength * radiusRatio;
+
+  ctx.fillRect(xCenter - radius, yCenter - radius, 2 * radius, 2 * radius);
+}
+
+function makeCode() {
   // Grab url input
   elementText = document.getElementById("text");
-  url = elementText.value
-  
+  url = elementText.value;
+
   // Check for non-empty url
   if (!url) {
     alert("Error: empty input");
@@ -21,44 +65,50 @@ function makeCode () {
   }
 
   // Pad URL since we want more density
-  maxLength = 40
+  maxLength = 40;
   if (url.length < maxLength) {
-    url += '?/' + '0'.repeat(maxLength - url.length)
-  }
+    url += "?/" + "0".repeat(maxLength - url.length);
+  } 
+  // else {
+  //   alert("Input too long");
+  //   elementText.focus();
+  //   return;    
+  // }
 
+  // Generate URL bits
   qrcode.makeCode(url);
 
   // Manually draw canvas
   QRMatrix = qrcode._oQRCode.modules;
-  QRLength = QRMatrix.length
+  QRLength = QRMatrix.length;
 
   // Form canvas
   canvas = document.getElementById("myCanvas");
   ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#DDDDDD";
-  canvasLength = 10*QRLength
-  canvas.width  = canvasLength;
+
+  // QR code sizing
+  bitLength = 10;
+  canvasLength = bitLength * QRLength;
+  canvas.width = canvasLength;
   canvas.height = canvasLength;
+
+  // Set background of canvas
+  ctx.fillStyle = "#DDDDDD";
   ctx.fillRect(0, 0, canvasLength, canvasLength);
-  boxLength = canvasLength/QRLength
 
+  // Colors of true and false bits
   black = "#000000";
-  white = "#FFFFFF"
+  white = "#FFFFFF";
 
-  // Populate canvas
+  // Populate canvas with bits
   for (let i = 0; i < QRLength; i++) {
-
     for (let j = 0; j < QRLength; j++) {
-
       if (QRMatrix[i][j]) {
         ctx.fillStyle = black;
       } else {
         ctx.fillStyle = white;
       }
-      ctx.fillRect(boxLength*i, boxLength*j, boxLength*(i+1), boxLength*(j+1));
-
+      drawShape(ctx, i, j, bitLength, 0.25, QRLength);
     }
-
   }
 }
-
