d3fd9828aea3fee15ef3504a5d15bb255cf1acdb
Hugh Chen <hugh.chen1@gmail.com>
2022-01-22 19:22:52 -0800

    Completed app

--- index.js
+++ index.js
@@ -1,10 +1,71 @@
+// Upload image to canvas
+var imageLoader = document.getElementById("imageLoader");
+imageLoader.addEventListener("change", handleImage, false);
+var uploadCanvas = document.getElementById("imageCanvas");
+var uploadContext = uploadCanvas.getContext("2d");
+
+img = false;
+var canvas = false;
+
+function handleImage(e) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+    img = new Image();
+    img.onload = function () {
+      uploadCanvas.width = img.width;
+      uploadCanvas.height = img.height;
+      uploadContext.drawImage(img, 0, 0);
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+}
+
+
 var qrcode = new QRCode("qrcode", {
   width: 256,
   height: 256,
   colorDark: "#000000",
   colorLight: "#ffffff",
-  correctLevel: QRCode.CorrectLevel.H,
+  correctLevel: QRCode.CorrectLevel.L,
 });
+
+
+var sizeSlider = document.getElementById("radiusSize");
+var sizeOutput = document.getElementById("printSize");
+sizeOutput.innerHTML = sizeSlider.value; // Display the default slider value
+var radiusRatio = sizeSlider.value/200;
+
+// Update the current slider value (each time you drag the slider handle)
+sizeSlider.oninput = function () {
+  sizeOutput.innerHTML = this.value;
+  radiusRatio = this.value/200;
+};
+
+var correctionSlider = document.getElementById("errorCorrection");
+var correctionOutput = document.getElementById("printCorrection");
+correctionOutput.innerHTML = correctionSlider.value; // Display the default slider value
+var correctionLevel = correctionSlider.value;
+if (correctionLevel === '1') {
+  qrcode._htOption.correctLevel = QRCode.CorrectLevel.L;
+} else if (correctionLevel === '2') {
+  qrcode._htOption.correctLevel = QRCode.CorrectLevel.M;
+} else if (correctionLevel === '3') {
+  qrcode._htOption.correctLevel = QRCode.CorrectLevel.H;
+} 
+
+// Update the current slider value (each time you drag the slider handle)
+correctionSlider.oninput = function () {
+  correctionOutput.innerHTML = this.value;
+  correctionLevel = correctionSlider.value;
+  if (correctionLevel === '1') {
+    qrcode._htOption.correctLevel = QRCode.CorrectLevel.L;
+  } else if (correctionLevel === '2') {
+    qrcode._htOption.correctLevel = QRCode.CorrectLevel.M;
+  } else if (correctionLevel === '3') {
+    qrcode._htOption.correctLevel = QRCode.CorrectLevel.H;
+  } 
+};
 
 /**
  * Check whether bit at current position should be full sized.
@@ -15,7 +76,6 @@ var qrcode = new QRCode("qrcode", {
  * @return {isPosition} Whether or not the current bit is safe to modify.
  */
 function isSafeBit(i, j, QRLength) {
-
   // Currently hard coding position bits
   if (i < 8 && j < 8) {
     return false;
@@ -23,7 +83,7 @@ function isSafeBit(i, j, QRLength) {
     return false;
   } else if (i < 8 && j > QRLength - 9) {
     return false;
-  } 
+  }
 
   return true;
 }
@@ -52,6 +112,18 @@ function drawShape(ctx, i, j, bitLength, radiusRatio, QRLength) {
   ctx.fillRect(xCenter - radius, yCenter - radius, 2 * radius, 2 * radius);
 }
 
+function download() {
+  // Download image
+  if (!canvas) {
+    alert("Error: no QR code to download");
+    return;
+  }
+  var link = document.getElementById('link');
+  link.setAttribute('download', 'qr_image.png');
+  link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+}
+
 function makeCode() {
   // Grab url input
   elementText = document.getElementById("text");
@@ -68,12 +140,7 @@ function makeCode() {
   maxLength = 40;
   if (url.length < maxLength) {
     url += "?/" + "0".repeat(maxLength - url.length);
-  } 
-  // else {
-  //   alert("Input too long");
-  //   elementText.focus();
-  //   return;    
-  // }
+  }
 
   // Generate URL bits
   qrcode.makeCode(url);
@@ -93,8 +160,11 @@ function makeCode() {
   canvas.height = canvasLength;
 
   // Set background of canvas
-  ctx.fillStyle = "#DDDDDD";
+  ctx.fillStyle = "#FFFFFF";
   ctx.fillRect(0, 0, canvasLength, canvasLength);
+  if (img) {
+    ctx.drawImage(img, 0, 0, canvasLength, canvasLength);
+  }
 
   // Colors of true and false bits
   black = "#000000";
@@ -108,7 +178,7 @@ function makeCode() {
       } else {
         ctx.fillStyle = white;
       }
-      drawShape(ctx, i, j, bitLength, 0.25, QRLength);
+      drawShape(ctx, i, j, bitLength, radiusRatio, QRLength);
     }
   }
 }
